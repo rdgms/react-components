243136158fa0fa0453105bcfef2900708fbc5ca5
Rodrigo <rodrigo.cofee@gmail.com>
2014-10-10 11:47:12 -0300

    added reactive-element support

--- components/my-button/index.jsx
+++ components/my-button/index.jsx
@@ -1,6 +1,10 @@
-var my_button = React.createClass({
+var MyButton = React.createClass({
 	
 	render: function () {	
 		return <button className="ola"> Hello </button>	
 	}
 });
+
+document.registerReact('my-button', MyButton);
+
+ 
