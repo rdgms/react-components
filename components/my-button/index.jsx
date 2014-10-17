b8730c2ac2d6513b54f14f5bd3e2679d357eecc8
Rafael Abensur <rabensur@verios.com.br>
2014-10-17 17:26:49 -0300

    naming convention

--- components/my-button/index.jsx
+++ components/my-button/index.jsx
@@ -1,10 +1,10 @@
-var MyButton = React.createClass({
+var VButton = React.createClass({
 	
 	render: function () {	
 		return <button className="ola"> Hello </button>	
 	}
 });
 
-document.registerReact('my-button', MyButton);
+document.registerReact('v-button', VButton);
 
  
---
+++ components/navbar/index.jsx
@@ -0,0 +1,10 @@
+var Navbar = React.createClass({
+	
+	render: function () {	
+		return <div className="navbar"></div>	
+	}
+});
+
+document.registerReact('v-navbar', Navbar);
+
+ 
